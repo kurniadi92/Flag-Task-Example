09a5c526cef1aee29def027545bd8234a59a4630
kurniadi <krniadi@hotmail.com>
2021-02-15 09:52:46 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/taskexample/MyApplication.kt
@@ -0,0 +1,19 @@
+package com.example.taskexample
+
+import android.app.ActivityManager
+import android.app.Application
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+
+
+class MyApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
---
+++ app/src/main/java/com/example/taskexample/SingleTaskActivity.kt
@@ -0,0 +1,85 @@
+package com.example.taskexample
+
+import android.app.ActivityManager
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class SingleTaskActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_task)
+        val singleTaskAffinityButton = findViewById<Button>(R.id.singleTaskWithAffinityButton)
+        val singleTaskButton = findViewById<Button>(R.id.singleTaskWithoutAffinityButton)
+        val aButton = findViewById<Button>(R.id.AActvityButton)
+        val bButton = findViewById<Button>(R.id.BActivityButton)
+        val cButton = findViewById<Button>(R.id.CActivityButton)
+        val dButton = findViewById<Button>(R.id.DActivityButton)
+        val mainButton = findViewById<Button>(R.id.MainButton)
+
+        singleTaskAffinityButton.setOnClickListener {
+            val intent = Intent(this, AffinitySingleTaskActivity::class.java)
+            startActivity(intent)
+        }
+
+        singleTaskButton.setOnClickListener {
+            val intent = Intent(this, SingleTaskActivity::class.java)
+            startActivity(intent)
+        }
+
+        aButton.setOnClickListener {
+            val intent = Intent(this, AActivity::class.java)
+            startActivity(intent)
+        }
+
+        bButton.setOnClickListener {
+            val intent = Intent(this, BActivity::class.java)
+            startActivity(intent)
+        }
+
+        cButton.setOnClickListener {
+            val intent = Intent(this, CActivity::class.java)
+            startActivity(intent)
+        }
+
+        dButton.setOnClickListener {
+            val intent = Intent(this, DActivity::class.java)
+            startActivity(intent)
+        }
+
+        mainButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        activityManager = getSystemService(ACTIVITY_SERVICE) as ActivityManager
+        getAppTaskState()
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        getAppTaskState()
+    }
+
+    protected var activityManager: ActivityManager? = null
+
+    fun getNumberOfTasks(): Int {
+        return activityManager!!.getAppTasks().size
+    }
+
+    fun getAppTaskState() {
+        println(">>>>>\n>>>>>\n")
+        println(">>>>> number of task ${getNumberOfTasks()}")
+        val totalNumberOfTasks: Int = activityManager!!.getRunningTasks(10).size //Returns total number of tasks - stacks
+        println("Total Number of Tasks: $totalNumberOfTasks")
+        val taskInfo: List<ActivityManager.RunningTaskInfo> =  activityManager!!.getRunningTasks(10) //returns List of RunningTaskInfo - corresponding to tasks - stacks
+        for (info in taskInfo) {
+            println(">>>>>>> ##########")
+            println(">>>>>>> Number of Activities \${info.id} " + info.numActivities)
+            println(">>>>>>> TOP Activity ${info.topActivity!!.className}")
+            println(">>>>>>> Base Activity ${info.baseActivity!!.className}")
+        }
+    }
+
+}
